7adfa1b5febe10758e932188d8fc56742ef137ea
Aziz uulu Amanbek <aibekdandaevv@gmail.com>
2025-11-05 23:58:20 +0600

    working with body of request

--- src/main/java/com/dandaev/edu/controllers/IndexServlet.java
+++ src/main/java/com/dandaev/edu/controllers/IndexServlet.java
@@ -1,7 +1,8 @@
 package com.dandaev.edu.controllers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Enumeration;
+import java.util.stream.Stream;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -13,24 +14,15 @@ public class IndexServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Enumeration<String> parameterNames = req.getParameterNames();
-
-		System.out.println("\n---=== PARAMETERS ===---");
-		while (parameterNames.hasMoreElements()) {
-			String name = parameterNames.nextElement();
-			String[] values = req.getParameterValues(name);
-
-			System.out.print(name + " : ");
-			if (values != null) {
-				for (String value : values) {
-					System.out.print(value + " ");
-				}
-				System.out.println();
-			}
-		}
-
 		RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.html");
 		requestDispatcher.forward(req, resp);
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try (BufferedReader reader = req.getReader();
+				Stream<String> lines = reader.lines();) {
+			lines.forEach(System.out::println);
+		}
+	}
 }
